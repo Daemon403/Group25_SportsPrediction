0d4cfeabfb9703e2ad9f9e96fae5c138be0ff23f
Daemon <103118323+Daemon403@users.noreply.github.com>
2023-10-22 20:41:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask,render_template, request, jsonify
 import pickle,joblib
 import numpy as np
@@ -26,33 +25,4 @@ def submit():
 
 
 if __name__ =="__main__":
-=======
-from flask import Flask,render_template, request, jsonify
-import pickle,joblib
-import numpy as np
-
-app = Flask(__name__)
-model = pickle.load(open('./model.pkl', 'rb')) 
-#model = joblib.load(open('./modelfinal.joblib', 'rb'))
-
-@app.route("/")
-def predict():
-    return render_template("index.html")
-
-
-@app.route("/sub", methods = ["POST"])
-def submit():
-    if request.method == "POST":
-        features = [int(x) for x in request.form.values()]
-        features = [np.array(features)]
-        #test = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-        # test = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-        # test = [np.array(test)]
-        final = model.predict(features)
-        final = round(final[0],0)
-        return render_template("sub.html", fini = final)
-
-
-if __name__ =="__main__":
->>>>>>> 4101dabcc99d01165dfa92f4533218e2f18076da
-    app.run(debug=True) 
+    app.run() 
